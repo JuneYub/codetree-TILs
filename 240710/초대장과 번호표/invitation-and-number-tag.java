8252953df4e05c99a772eaea20a5a13b3a87e006
JuneYub <91084695+JuneYub@users.noreply.github.com>
2024-07-10 09:49:34 +0900

    Title: 초대장과 번호표, Time: 86ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240710/초대장과 번호표/invitation-and-number-tag.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static final int MAX_N = 250000;
+
+    public static int n, g;
+
+    public static HashSet<Integer> total = new HashSet<>();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        g = Integer.parseInt(st.nextToken());
+
+        HashSet<Integer>[] array = new HashSet[g];
+
+        for(int i = 0; i < g; i++) {
+            st = new StringTokenizer(br.readLine());
+            array[i] = new HashSet<>();
+            int cnt = Integer.parseInt(st.nextToken());
+
+            for(int j = 0; j < cnt; j++) {
+                int num = Integer.parseInt(st.nextToken());
+                if(j == 0) {
+                    total.add(num);
+                }
+                array[i].add(num);
+            }
+        }
+
+        Arrays.sort(array, new Comparator<HashSet<Integer>>(){
+            public int compare(HashSet<Integer> a, HashSet<Integer> b) {
+                if(a == null && b == null) return 0;
+                if(a == null) return -1;
+                if(b == null) return 1;
+                return a.size() - b.size();
+            }
+        });
+
+        while(true) {
+
+            int orgSize = total.size();
+            for(HashSet<Integer> set : array) {
+                if(set != null) {
+                    set.removeAll(total);
+                    if (set.size() == 1) {
+                        total.addAll(set);
+                    }
+                }
+            }
+
+            if(orgSize == total.size()) {
+                break;
+            }
+        }
+
+        System.out.println(total.size());
+
+    }
+}
