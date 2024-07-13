5a0a293efc278f0acbb566b5f30f3151ee3ab65d
JuneYub <91084695+JuneYub@users.noreply.github.com>
2024-07-13 19:21:45 +0900

    Title: 배열 추출 2, Time: 361ms, Memory: 22MB, Status: Passed - CodeTree

--- 240713/배열 추출 2/array-extraction-2.java
+++ 240713/배열 추출 2/array-extraction-2.java
@@ -1,6 +1,25 @@
 import java.io.*;
 import java.util.*;
 
+class Value implements Comparable<Value> {
+    int absValue;
+    int orgValue;
+
+    Value(int orgValue, int absValue) {
+        this.orgValue = orgValue;
+        this.absValue = absValue;
+    }
+
+    public int compareTo(Value v) {
+        if(this.absValue != v.absValue) {
+            return this.absValue - v.absValue;
+        } else {
+            return this.orgValue - v.orgValue;
+        }
+    }
+
+}
+
 public class Main {
     public static int n;
 
@@ -8,18 +27,18 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        PriorityQueue<Value> pq = new PriorityQueue<>();
         n = Integer.parseInt(br.readLine());
         
         for(int i = 0; i < n; i++) {
             int x = Integer.parseInt(br.readLine());
             if(x != 0) {
-                pq.add(x);
+                pq.add(new Value(x, Math.abs(x)));
             } else {
                 if(pq.isEmpty()) {
                     sb.append(0 + "\n");
                 } else {
-                    sb.append(pq.poll() + "\n");
+                    sb.append(pq.poll().orgValue + "\n");
                 }
                 
             }
