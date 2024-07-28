8498a05440d05619318e7c480b7e08fe2449b78a
JuneYub <91084695+JuneYub@users.noreply.github.com>
2024-07-29 00:27:48 +0900

    Title: 겹치는 선분들, Time: 606ms, Memory: 27MB, Status: Passed - CodeTree

---
+++ 240729/겹치는 선분들/overlapping-lines.java
@@ -0,0 +1,91 @@
+import java.io.*;
+import java.util.*;
+
+class Point implements Comparable<Point> {
+    int x, v, idx;
+
+    public Point(int x, int v) {
+        this.x = x;
+        this.v = v;
+    }
+
+    public int compareTo(Point p) {
+    	if(this.x == p.x) {
+    		return this.v - p.v;
+    	}
+        return this.x - p.x;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        List<Point> points = new ArrayList<>();
+
+        int startPoint = 0;
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int num = Integer.parseInt(st.nextToken());
+            String str = st.nextToken();
+            char oper = str.charAt(0);
+
+            if(oper == 'R') {
+                int end = startPoint + num;
+                points.add(new Point(startPoint, 1));
+                points.add(new Point(end, -1));
+
+                startPoint = end;
+
+            } else {
+                int start = startPoint - num;
+                points.add(new Point(start, 1));
+                points.add(new Point(startPoint, -1));
+
+                startPoint = start;
+            }
+        }
+
+        Collections.sort(points);
+
+        int result = 0;
+        int cnt = 0;
+        int startIdx = 0;
+        for(int i = 0; i < points.size(); i++) {
+            
+
+            Point p = points.get(i);
+            if(p.v == 1) {
+                cnt++;
+                if(cnt >= k && cnt -1 < k) {
+                    startIdx = p.x;
+                }
+            } else {
+
+                if(cnt >= k) {
+
+                    
+                    if(i+1 < points.size()) {
+                        if(points.get(i+1).x == p.x && points.get(i+1).v == -1 && (cnt -1) >= k) {
+                            cnt--;
+                        	continue;
+                        }
+                    }
+
+                    //System.out.println(p.x + " " + startIdx);
+                    result += Math.abs(p.x - startIdx);
+                }
+                cnt--;
+                if(cnt >= k) {
+                	startIdx = p.x;
+                }
+            }
+        }
+        System.out.print(result);
+
+    }
+}
