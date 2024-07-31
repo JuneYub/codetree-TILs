7257752e60629860dba92575c7aa83258d7e0b2f
JuneYub <91084695+JuneYub@users.noreply.github.com>
2024-08-01 08:44:56 +0900

    Title: 최소 에너지 비용, Time: 194ms, Memory: 13MB, Status: Passed - CodeTree

--- 240801/최소 에너지 비용/minimum-energy-cost.java
+++ 240801/최소 에너지 비용/minimum-energy-cost.java
@@ -6,10 +6,9 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        int[] need = new int[n];
+        int[] need = new int[n-1];  // 크기를 n-1로 변경
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 0; i < n-1; i++) {
-            // 2 3 1 0 이 저장된다.
             need[i] = Integer.parseInt(st.nextToken());
         }
 
@@ -20,18 +19,14 @@ public class Main {
             charge[i] = Integer.parseInt(st.nextToken());
         }
 
-        // 앞에서부터 가장 에너지가 적게 드는 충전소를 최신하하며 저장한다.
         minCharge[0] = charge[0];
         for(int i = 1; i < n; i++) {
-            // 5 2 2 1 이런식임
             minCharge[i] = Math.min(charge[i], minCharge[i-1]);
         }
 
-        // 이제 필요한 에너지를 구한다. need에서 필요한 에너지와 minCharge 를 곱한 값을 누적한다.
-        long ans = 0;
+        long ans = 0;  // ans만 long으로 선언
         for(int i = 0; i < n-1; i++) {
-            //System.out.println(minCharge[i] + " " + need[i]);
-            ans += (minCharge[i] * need[i]);
+            ans += (long)minCharge[i] * need[i];  // 곱셈 결과를 long으로 캐스팅
         }
         System.out.print(ans);
     }
