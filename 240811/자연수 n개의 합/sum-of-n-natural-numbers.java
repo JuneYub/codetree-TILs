43b193153ddddbb2afca7b706b9e725860c01f9c
JuneYub <91084695+JuneYub@users.noreply.github.com>
2024-08-11 01:37:22 +0900

    Title: 자연수 n개의 합, Time: 166ms, Memory: 8MB, Status: Passed - CodeTree

--- 240811/자연수 n개의 합/sum-of-n-natural-numbers.java
+++ 240811/자연수 n개의 합/sum-of-n-natural-numbers.java
@@ -7,12 +7,13 @@ public class Main {
         long s  = Long.parseLong(br.readLine());
 
         long left = 0;
-        long right = s + 1;
+        long right = 2_000_000_000;
         long maxIdx = 0;
         while(left <= right) {
             long mid = (left + right) / 2;
             long sum = (mid*(mid+1))/2;
-            if(sum <= s) {
+            //System.out.println(mid + " " + sum);
+            if(sum > 0 && sum <= s) {
                 maxIdx = Math.max(maxIdx, mid);
                 left = mid + 1;
             } else {
