5aa7ff6da6b979a32a3353edee016c75ce9b8602
JuneYub <91084695+JuneYub@users.noreply.github.com>
2024-08-13 08:22:11 +0900

    Title: 최소 통과 시간, Time: 153ms, Memory: 13MB, Status: Wrong Answer - CodeTree

---
+++ 240813/최소 통과 시간/minimum-transit-time.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    static int n, m;
+    static long[] arr;
+    public static void main(String[] args) throws Exception {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new long[m];
+        
+        for(int i = 0; i < m; i++) {
+            arr[i] = Long.parseLong(br.readLine());
+        }
+
+        long left = 1;
+        long right = 1_000_000_000;
+        long midIdx = 1_000_000_000;
+        while(left <= right) {
+            long mid = (left +right) / 2;
+
+            if( isPossible(mid) ) {
+                right = mid -1;
+                midIdx = Math.min(midIdx, mid);
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        System.out.print(midIdx);
+    }
+
+    public static boolean isPossible(long target) {
+        long cnt = 0;
+        for(int i  = 0; i < m; i++) {
+            cnt += (target / arr[i]);
+        }
+        if(cnt >= n) return true;
+        return false;
+    }
+}
